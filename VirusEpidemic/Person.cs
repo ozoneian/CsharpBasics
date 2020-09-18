ebc6d61ab4562aaeb8df06fa566d058eb1df148d
ozoneian <71430787+ozoneian@users.noreply.github.com>
2020-09-18 12:11:29 +0200

    Person class with properties added. Methods added. Additional notes: hard coded, uncomplete runconditional, could benefit from moving things around.

---
+++ VirusEpidemic/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirusEpidemic
+{
+    public class Person
+    {
+
+        public bool Infected { get; set; }
+        public int InfectedWhen { get; set; }
+
+        public bool Immune { get; set; }
+
+        public List<Person> CreatePeople()
+        {
+            List<Person> disco = new List<Person>
+            {
+                PatientZero()
+            };
+
+            Console.WriteLine("How many people are allowed in the disco?\nInput number: ");
+            int peopleAllowedIntoDisco = int.Parse(Console.ReadLine());
+            peopleAllowedIntoDisco -= 1;
+
+            for (int i = 0; i < peopleAllowedIntoDisco; i++)
+            {
+                Person PersonInDisco = new Person();
+                disco.Add(PersonInDisco);
+            }
+            
+            return disco;
+        }
+        public Person PatientZero()
+        {
+            Person person = new Person()
+            {
+                Infected = true,
+            };
+            return person;
+        }
+
+    }
+}
--- VirusEpidemic/Program.cs
+++ VirusEpidemic/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace VirusEpidemic
 {
@@ -6,7 +9,71 @@ namespace VirusEpidemic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RunDisco();
         }
+        static void RunDisco()
+        {
+            int discoOpenedFor = 0;
+            bool discoOpen = true;
+
+            Person person = new Person();
+            List<Person> people = person.CreatePeople();
+            while (discoOpen)
+            {
+                Console.Clear();
+                int counter = 0;
+
+                for (int i = 0; i < people.Count; i++)
+                {
+                    Console.WriteLine($"Disco has been opened for {discoOpenedFor} h. Infected: {people[i].Infected} , Infected for: {people[i].InfectedWhen} , Immune: {people[i].Immune}");
+
+
+                    if (people[i].Infected)
+                    {
+                        SetInfectionDuration(people[i]);
+                        counter++;
+                    }
+                }
+                SetInfected(people,counter);
+                
+
+                discoOpenedFor++;
+                
+                Console.ReadKey(true);
+            }
+        }
+        static void SetInfected(List<Person> people, int amount) //Breakpoint can't big iq this
+        {
+            int temp = 0;
+
+            foreach (var person in people)
+            {
+                if (person.Infected==false && person.Immune==false && temp<amount)
+                {
+                    person.Infected = true;
+                    temp++;
+                }
+            }
+        }
+        static void SetInfectionDuration(Person person)
+        {
+            if (person.InfectedWhen == 5)
+            {
+                MakeImmune(person);
+
+            }
+            else if (person.InfectedWhen<5)
+            {
+                person.InfectedWhen++;
+            }
+            
+        }
+        static void MakeImmune(Person person)
+        {
+            person.Immune = true;
+            person.Infected = false;
+        }
+        
+       
     }
 }
