cb6db281ec238ece8d65a1ef281982cb84e7bcfd
ozoneian <71430787+ozoneian@users.noreply.github.com>
2020-09-21 13:59:24 +0200

    Completed Merry Methods asignment. Messy main method and poorly choosen names for parameters, variables etc.

---
+++ MerryMethods/MethodBench.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MerryMethods
+{
+    class MethodBench
+    {
+        public void FirstMethod()
+        {
+            Console.WriteLine("Merry Methods");
+        }
+        public void SecondMethod(string parameter)
+        {
+            Console.WriteLine(parameter);
+        }
+        public void ThirdMethod(string parameter, bool conditional)
+        {
+            if (conditional==true)
+            {
+                SecondMethod(parameter.ToUpper());
+            }
+            else
+            {
+                SecondMethod(parameter.ToLower());
+            }
+        }
+        public bool FourthMethod()
+        {
+            Console.WriteLine("Do you want to 'whisper' or 'scream'?\n\n Press the key [W] to whisper OR [S] to scream: ");
+            if (Console.ReadKey(true).Key == ConsoleKey.S)
+            {
+                return true;
+            }
+            //else if (Console.ReadKey(true).Key==ConsoleKey.W)
+            //{
+            //    return false;
+            //}
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MerryMethods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MerryMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodBench methodBench = new MethodBench();
+            bool shout = methodBench.FourthMethod();
+            int conditional = 0;
+            
+
+            do
+            {
+                methodBench.FirstMethod();
+                conditional++;
+
+            } while (conditional < 3);
+
+            methodBench.SecondMethod("C# Now and Forever!");
+            string userSentance = Console.ReadLine();
+            methodBench.SecondMethod(userSentance);
+            
+            bool makeChoice = true;
+            bool isLargeSmall = false;
+            while (makeChoice)
+            {
+                Console.WriteLine("Type 'small' for the output to be in small letters or 'large' for large letters!: ");
+                string largeSmall = Console.ReadLine();
+
+                if (largeSmall == "large")
+                {
+                    isLargeSmall = true;
+                    makeChoice = false;
+                }
+                else if (largeSmall == "small")
+                {
+                    isLargeSmall = false;
+                    makeChoice = false;
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Type large/small! ");
+                }
+
+            }
+            methodBench.ThirdMethod(userSentance, isLargeSmall);
+            methodBench.ThirdMethod(userSentance, shout);
+            methodBench.ThirdMethod(userSentance, methodBench.FourthMethod());
+        }
+    }
+}
